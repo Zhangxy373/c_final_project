517a37bb75b6ccae946b89975f5b5c9495722500
Zhangxy373 <zxy17757707133@163.com>
2023-12-24 15:19:12 +0800

    advance中的txt文件存在问题

--- advanced.cpp
+++ advanced.cpp
@@ -28,6 +28,19 @@ static void calc_bmi(struct Person_type* ptr, char n_people)
 	// The BMI values and health conditions (in terms of weight ranks) can be evaluated 
 	// according to Equation (1) and Table 1, respectively, as in the slides.
 	// You can use the strcpy_s() function to copy the strings of health conditions into the structures.
+
+	for (int i = 0; i < n_people; i++) {
+		// Calculating BMI
+		ptr[i].bmi = ptr[i].weight / pow((ptr[i].height / 100), 2);
+
+		// Determining health condition based on BMI
+		if (ptr[i].bmi < 18.5) strcpy_s(ptr[i].health_condition, "Underweight");
+		else if (ptr[i].bmi < 24) strcpy_s(ptr[i].health_condition, "Normal");
+		else if (ptr[i].bmi < 27) strcpy_s(ptr[i].health_condition, "Overweight");
+		else if (ptr[i].bmi < 30) strcpy_s(ptr[i].health_condition, "Mild Obesity");
+		else if (ptr[i].bmi < 35) strcpy_s(ptr[i].health_condition, "Moderate Obesity");
+		else strcpy_s(ptr[i].health_condition, "Severe Obesity");
+
 }
 
 
@@ -38,9 +51,10 @@ static void calc_overweight(struct Person_type* ptr, char n_people)
 	* ptr	   -> the structure pointer.
 	* n_people -> the number of people.
 	*/
-
-	//Your code
-
+	for (int i = 0; i < n_people; i++) {
+		float standard_weight = (ptr[i].gender == 'M') ? 22 * pow((ptr[i].height / 100), 2) : 21 * pow((ptr[i].height / 100), 2);
+		ptr[i].overweight = ((ptr[i].weight - standard_weight) / standard_weight) * 100;
+	}
 }
 
 
@@ -53,6 +67,15 @@ static void ascending_sorting(struct Person_type** arr, char n_people)
 	*/
 	
 	// Sort the pointer array using methods like bubble/selection sorting.
+	for (int i = 0; i < n_people - 1; i++) {
+		for (int j = 0; j < n_people - i - 1; j++) {
+			if (arr[j]->bmi > arr[j + 1]->bmi) {
+				struct Person_type* temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
 }
 
 // File reading function
@@ -106,8 +129,7 @@ static void write_file(struct Person_type** arr, char n_people)
 }
 
 
-int main()
-{
+int main(){
 
 	char n_people = 8;
 	char n_name = 10;
@@ -116,23 +138,26 @@ int main()
 	struct Person_type people_arr[8];
 
 	// read the file to get data
-	read_file();
+	read_file(people_arr, n_people, n_name);
 
 	// calculate the BMI values for each person
-	calc_bmi();
+	calc_bmi(people_arr, n_people);
 
 	// calculate the overweight percentage value
-	calc_overweight();
+	calc_overweight(people_arr, n_people);
 
 	// sort the people according to the BMI values
 	// define a pointer array
 	// assign values for the pointer array
 	// sort the pointer array based on the BMI values
 	struct Person_type* sorted_arr[8];
-	ascending_sorting();
+	for (int i = 0; i < n_people; i++) {
+		sorted_arr[i] = &people_arr[i];
+	}
+	ascending_sorting(sorted_arr, n_people);
 
 	// write the file to save the sorted structure array
-	write_file();
+	write_file(sorted_arr, n_people);
 
 	return 0;
 }
